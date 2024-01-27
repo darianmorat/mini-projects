50a7ff291debfe489acb28ec63e467011ea0785f
darianmorat <yojhandariantoledomora@gmail.com>
2024-01-27 11:52:35 -0500

    Update crash logic for game

--- etch-sketch/main.js
+++ etch-sketch/main.js
@@ -1,9 +1,27 @@
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = 'repeat(16, 1fr)';
-board.style.gridTemplateRows = 'repeat(16, 1fr)';
+function populateBoard(size) {
+   let board = document.querySelector('.board');
+   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for (let i = 0; i < 256; i++) {
-   let square = document.createElement('div');
-   square.style.backgroundColor = 'blue';
-   board.insertAdjacentElement('beforeend', square);
+   let amount = size * size;
+   for (let i = 0; i < amount; i++) {
+      let square = document.createElement('div');
+      square.style.backgroundColor = 'blue';
+      board.insertAdjacentElement('beforeend', square);
+   }
+}
+populateBoard(16); // default value
+
+function changeSize(userInput) {
+   if (userInput >= 2 || userInput <= 50) {
+      populateBoard(userInput);
+   }
+   if (userInput < 2) {
+      alert('You need more to draw!');
+      populateBoard(2);
+   }
+   if (userInput > 50) {
+      alert('WAY TO MUCH!');
+      populateBoard(50);
+   }
 }
